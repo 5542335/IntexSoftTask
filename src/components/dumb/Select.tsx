39d163b94676cc3dc84bf0bef529e52fe523fcc0
ABosko <alexey_diary@mail.ru>
2022-01-05 20:18:47 +0300

    refact select component

--- src/components/dumb/Select.tsx
+++ src/components/dumb/Select.tsx
@@ -1,33 +1,48 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 
-export const StyledSelect = styled.select`
-    font-family: Lato;
+export const StyledText = styled.p`
+    font-family: Lato, Arial, sans-serif;
     font-style: normal;
     font-weight: bold;
     font-size: 14px;
     line-height: 17px;
+    letter-spacing: 0.02em;
+`;
+
+interface StyledSelectProps {
+    hide?: boolean;
+}
+
+export const StyledSelect = styled.div<StyledSelectProps>`
+    display: flex;
+    max-width: 270px;
+    min-height: 48px;
+    justify-content: center;
+    align-items: center;
+    
     background-color: #FFFFFF;
+    color: #3386D9;
     border: 1px solid #D0E2F6;
     box-sizing: border-box;
     border-radius: 8px;
-    padding: 16px 55px;
-    max-width: 270px;
-    color: #3386D9;
-    outline: none;
-    &:hover {
-        cursor: pointer;
-        border: 2px solid 
+    cursor: pointer;
+    :hover {
+        border-color: #3386D9;
     }
+    display: ${props => props.hide ? 'none' : ''};
 
-    option {
-        color: #3386D9;
-        background-color: #f1f1f1;
+    ${StyledText} {
+        margin-right: 14px;
     }
-    option:checked {
-        background: #808080;
-        color: #ffffff;
-}
+`;
+
+export const StyledOptionsWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 270px;
+    max-height: 144px;
+    overflow-x: hidden;
 `;
 
 interface SelectProps {
@@ -36,21 +51,36 @@ interface SelectProps {
     onChange: (value: string) => void;
   }
 
-
 export const Select: FC<SelectProps> = ({currentSelect, items, onChange}: SelectProps) => {
+    const [isOpenOptions, setIsOpenOptions] = useState(false);
+
+const handleClickOption = (selectedOption: string) => () => {
+    onChange(selectedOption || 'Choose department');
+    setIsOpenOptions(false);
+};
+
     return (
-        <StyledSelect onChange={(e) => onChange(e.target.value)}>
-            <option hidden>Choose department</option>
-            {items.map((department) => {
-               return (
-                    <option
-                        key={department}
-                        hidden={currentSelect === department}
-                    >
-                        {department}
-                    </option>
-               )
-            })}
-        </StyledSelect>
+        <>
+            <StyledSelect onClick={() => setIsOpenOptions(!isOpenOptions)}>
+                <StyledText>{currentSelect}</StyledText> 
+                <img src="arrowButton.png" alt='choose department button'/>
+            </StyledSelect>
+            <StyledOptionsWrapper>
+                {currentSelect !== 'Choose department'
+                    && isOpenOptions
+                    && <StyledSelect onClick={handleClickOption('')}>All</StyledSelect>
+                }
+                {isOpenOptions && items.map((department) => {
+                    return (
+                        <StyledSelect
+                            onClick={handleClickOption(department)}
+                            hide={department === currentSelect}
+                        >
+                            {department}
+                        </StyledSelect>
+                    )
+                })}
+            </StyledOptionsWrapper>
+        </>
     )
 }
