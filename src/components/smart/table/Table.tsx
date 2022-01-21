b670397d5ee1d1029dc386dbdf373556ebf73c89
ABosko <alexey_diary@mail.ru>
2022-01-21 21:59:20 +0300

    fix Table styles

--- src/components/smart/table/Table.tsx
+++ src/components/smart/table/Table.tsx
@@ -1,19 +1,8 @@
 import styled from 'styled-components';
 import { FC } from 'react';
-import { StyledTableHeader, TableHeader, TableHeaderProps } from './TableHeader';
+
+import { TableHeader, TableHeaderProps } from './TableHeader';
 import { TableBody, TableBodyProps } from './TableBody';
-import {
-  StyledBodyText,
-  StyledBuildingIcon,
-  StyledChip,
-  StyledDepartmentWrapper,
-  StyledLogo,
-  StyledLogoWrapper,
-  StyledNameWrapper,
-  StyledTrashIcon,
-  StyledWorkplaceWrapper,
-} from './employeeTableBody';
-import { StyledTableItem, StyledTableRow } from './TableRow';
 import { Pagination } from './Pagination';
 
 interface TableProps<T> extends TableBodyProps<T>, TableHeaderProps {
@@ -23,130 +12,6 @@ interface TableProps<T> extends TableBodyProps<T>, TableHeaderProps {
 export const StyledTable = styled.div`
   display: flex;
   flex-direction: column;
-  ${StyledChip} {
-    margin: 3px 0;
-    :not(:last-child) {
-      margin-right: 8px;
-    }
-  }
-
-  @media (max-width: 1110px) {
-    ${StyledTableRow} {
-      padding: 0 5px;
-    }
-  }
-
-  @media (min-width: 768px) {
-    margin-top: 24px;
-
-    ${StyledNameWrapper} {
-      margin-left: 12px;
-    }
-    ${StyledLogo} {
-      margin: 12px 0;
-    }
-
-    ${StyledTrashIcon} {
-      margin-left: 10px;
-    }
-  }
-
-  @media (min-width: 480px) and (max-width: 768px) {
-    margin-top: 24px;
-    ${StyledLogoWrapper} {
-      position: absolute;
-      left: 5px;
-      top: 5px;
-    }
-    ${StyledNameWrapper} {
-      align-items: flex-start;
-      margin-top: 40px;
-    }
-    ${StyledDepartmentWrapper} {
-      min-width: 90px;
-    }
-    ${StyledTableRow} {
-      :not(:first-child) {
-        min-height: 80px;
-      }
-    }
-    ${StyledChip} {
-      padding: 8px 16px 8px 16px;
-    }
-    ${StyledTrashIcon} {
-      margin-left: 10px;
-    }
-  }
-
-  @media (max-width: 480px) {
-    ${StyledTableHeader} {
-      display: none;
-    }
-    ${StyledTableRow} {
-      display: grid;
-      grid-template-rows: 22px 22px 1fr;
-      grid-template-columns: 44px 2fr 1fr;
-      padding: 16px;
-      margin: 20px 16px;
-      background-color: #fff;
-      border: 1px solid #d0e2f6;
-      box-sizing: border-box;
-      border-radius: 8px;
-      ${StyledTableItem} {
-        //StyledLogoWrapper
-        :nth-child(1) {
-          grid-row: 1 / 3;
-          grid-column: 1;
-        }
-        //StyledNameWrapper
-        :nth-child(2) {
-          grid-row: 1;
-          grid-column: 2;
-        }
-        //StyledPositionWrapper
-        :nth-child(3) {
-          grid-row: 2;
-          grid-column: 2;
-          ${StyledBodyText} {
-            font-size: 12px;
-            line-height: 14px;
-          }
-        }
-        //StyledDepartmentWrapper
-        :nth-child(4) {
-          grid-row: 1;
-          grid-column: 3;
-          justify-self: flex-end;
-        }
-        //StyledWorkplaceWrapper
-        :nth-child(5) {
-          grid-row: 3;
-          grid-column: 1 / 4;
-          border-top: 1px solid #d0e2f6;
-          margin-top: 10px;
-        }
-      }
-    }
-    ${StyledWorkplaceWrapper} {
-      ${StyledBodyText} {
-        font-size: 12px;
-      }
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-    }
-    ${StyledChip} {
-      margin-top: 10px;
-      width: 100%;
-      padding: 12px 0 12px;
-    }
-    ${StyledTrashIcon} {
-      margin: 0 20px 0 auto;
-    }
-    ${StyledBuildingIcon} {
-      margin: 0 16px 0 20px;
-    }
-  }
 `;
 
 export const Table: FC<TableProps<any>> = ({ data, columns, columnTitles, paginationProps }) => {
--- src/views/EmployeePage.tsx
+++ src/views/EmployeePage.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-
 import { FC, useState } from 'react';
 
 import { StyledTitle, Title } from '../components/dumb/Title';
@@ -7,9 +6,22 @@ import { Tabs, StyledTabs } from '../components/dumb/Tabs';
 import { SearchBar, StyledSearchBar, StyledSearchBarWrapper } from '../components/smart/SearchBar';
 import { Select, StyledSelectWrapper, StyledSelect, StyledOptionsWrapper } from '../components/dumb/Select';
 import { workPlace, departments, users } from '../data';
-import { Table } from '../components/smart/table/Table';
-import { getEmployeeTableBody } from '../components/smart/table/employeeTableBody';
+import { StyledTable, Table } from '../components/smart/table/Table';
+import {
+  getEmployeeTableBody,
+  StyledBodyText,
+  StyledBuildingIcon,
+  StyledChip,
+  StyledDepartmentWrapper,
+  StyledLogo,
+  StyledLogoWrapper,
+  StyledNameWrapper,
+  StyledTrashIcon,
+  StyledWorkplaceWrapper,
+} from '../components/smart/table/employeeTableBody';
 import { getEmployeeTableTitles } from '../components/smart/table/employeeTableTitles';
+import { StyledTableItem, StyledTableRow } from '../components/smart/table/TableRow';
+import { StyledTableHeader } from '../components/smart/table/TableHeader';
 
 const StyledPageWrapper = styled.div`
   display: flex;
@@ -25,6 +37,24 @@ const StyledPageWrapper = styled.div`
     font-size: 36px;
     line-height: 43px;
   }
+  ${StyledTable} {
+    margin-top: 24px;
+  }
+  ${StyledLogo} {
+    margin: 12px 0;
+  }
+  ${StyledNameWrapper} {
+    margin-left: 12px;
+  }
+  ${StyledChip} {
+    margin: 3px 0;
+    :not(:last-child) {
+      margin-right: 8px;
+    }
+  }
+  ${StyledTrashIcon} {
+    margin-left: 10px;
+  }
 
   @media (max-width: 1110px) {
     ${StyledTabs} {
@@ -32,6 +62,37 @@ const StyledPageWrapper = styled.div`
     }
     ${StyledTitle} {
       margin-left: 5px;
+    }
+    ${StyledTableRow} {
+      padding: 0 5px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    ${StyledNameWrapper} {
+      align-items: flex-start;
+      margin-top: 40px;
+      margin-left: 0;
+    }
+    ${StyledChip} {
+      padding: 8px 16px 8px 16px;
+    }
+    ${StyledDepartmentWrapper} {
+      min-width: 100px;
+    }
+    ${StyledTableRow} {
+      :not(:first-child) {
+        min-height: 80px;
+      }
+    }
+    ${StyledLogo} {
+      margin: 0;
+      align-self: flex-start;
+    }
+    ${StyledLogoWrapper} {
+      position: absolute;
+      left: 5px;
+      top: 5px;
     }
   }
 
@@ -45,6 +106,89 @@ const StyledPageWrapper = styled.div`
     }
     ${StyledTabs} {
       box-shadow: 0 6px 12px 0 #0000001e;
+    }
+    ${StyledTable} {
+      margin-top: 0;
+    }
+    ${StyledChip} {
+      margin: 8px 0 0;
+      width: 100%;
+      padding: 12px 0 12px;
+      :not(:last-child) {
+        margin-right: 0;
+      }
+      :first-child {
+        margin-top: 12px;
+      }
+    }
+    ${StyledTableRow} {
+      display: grid;
+      grid-template-rows: 22px 22px 1fr;
+      grid-template-columns: 44px 2fr 1fr;
+      padding: 16px;
+      margin: 20px 16px;
+      background-color: #fff;
+      border: 1px solid #d0e2f6;
+      box-sizing: border-box;
+      border-radius: 8px;
+      :not(:first-child) {
+        min-height: 0;
+      }
+    }
+    ${StyledTableItem} {
+      :nth-child(1) {
+        grid-row: 1 / 3;
+        grid-column: 1;
+      }
+      :nth-child(2) {
+        grid-row: 1;
+        grid-column: 2;
+      }
+      :nth-child(3) {
+        grid-row: 2;
+        grid-column: 2;
+        ${StyledBodyText} {
+          font-size: 12px;
+          line-height: 14px;
+        }
+      }
+      :nth-child(4) {
+        grid-row: 1;
+        grid-column: 3;
+        justify-self: flex-end;
+      }
+      :nth-child(5) {
+        grid-row: 3;
+        grid-column: 1 / 4;
+        border-top: 1px solid #d0e2f6;
+        margin-top: 10px;
+      }
+    }
+    ${StyledTableHeader} {
+      display: none;
+    }
+    ${StyledNameWrapper} {
+      margin: 0;
+    }
+    ${StyledLogoWrapper} {
+      position: static;
+    }
+    ${StyledDepartmentWrapper} {
+      min-width: 0;
+    }
+    ${StyledWorkplaceWrapper} {
+      ${StyledBodyText} {
+        font-size: 12px;
+      }
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+    ${StyledTrashIcon} {
+      margin: 0 20px 0 auto;
+    }
+    ${StyledBuildingIcon} {
+      margin: 0 16px 0 20px;
     }
   }
 `;
